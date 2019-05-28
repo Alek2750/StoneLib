aba0dc74ca5249a9291426c9b0428a96bbe21305
Alek2750 <alek2750@gmail.com>
2019-05-28 11:46:01 +0200

    "Could not load properies for :pu.properties"

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -1,42 +1,48 @@
-//package utils;
-//
-//import entity.Role;
-//import entity.User;
-//import javax.persistence.EntityManager;
-//
-//public class SetupTestUsers {
-//
-//  public static void main(String[] args) {
-//
-//    EntityManager em = PuSelector.getEntityManagerFactory("pu").createEntityManager();
-//       // Make travis do a build
-//    // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//    // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
-//    // CHANGE the three passwords below, before you uncomment and execute the code below
-//    
-//    //throw new UnsupportedOperationException("REMOVE THIS LINE, WHEN YOU HAVE READ WARNING");
+package utils;
+
+import entity.Role;
+import entity.User;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class SetupTestUsers {
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+    
+    
+  public static void main(String[] args) {
+
+    //EntityManager em = PuSelector.getEntityManagerFactory("pu").createEntityManager();
+    EntityManager em = emf.createEntityManager();
+       // Make travis do a build
+    // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
+    // CHANGE the three passwords below, before you uncomment and execute the code below
+    
+    //throw new UnsupportedOperationException("REMOVE THIS LINE, WHEN YOU HAVE READ WARNING");
 //    No longer using these users
-//    em.getTransaction().begin();
-//    Role userRole = new Role("user");
-//    Role adminRole = new Role("admin");
-//    User user = new User("user", "user1234");
-//    user.addRole(userRole);
-//    User admin = new User("admin", "admin1234");
-//    admin.addRole(adminRole);
-//    User both = new User("user_admin", "both1234");
-//    both.addRole(userRole);
-//    both.addRole(adminRole);
-//    em.persist(userRole);
-//    em.persist(adminRole);
-//    em.persist(user);
-//    em.persist(admin);
-//    em.persist(both);
-//    em.getTransaction().commit();
-//    System.out.println("PW: " + user.getUserPass());
-//    System.out.println("Testing user with OK password: " + user.verifyPassword("user1234"));
-//    System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
-//    System.out.println("Created TEST Users");
-//   
-//  }
-//
-//}
+    em.getTransaction().begin();
+    Role userRole = new Role("user");
+    Role adminRole = new Role("admin");
+    User user = new User("user", "user1234");
+    user.addRole(userRole);
+    User admin = new User("admin", "admin1234");
+    admin.addRole(adminRole);
+    User both = new User("user_admin", "both1234");
+    both.addRole(userRole);
+    both.addRole(adminRole);
+    em.persist(userRole);
+    em.persist(adminRole);
+    em.persist(user);
+    em.persist(admin);
+    em.persist(both);
+    em.getTransaction().commit();
+    System.out.println("PW: " + user.getUserPass());
+    System.out.println("Testing user with OK password: " + user.verifyPassword("user1234"));
+    System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
+    System.out.println("Created TEST Users");
+   
+  }
+
+}
